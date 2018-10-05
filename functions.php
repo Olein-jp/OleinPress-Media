383db3cb1965536494ff9baf2c154ff1cccc3677
KojiKuno <koji.kuno@gmail.com>
2018-10-05 11:36:32 +0900

    bootstrapとfontawesomeをlibに読み込む

--- functions.php
+++ functions.php
@@ -68,8 +68,10 @@ add_action( 'widgets_init', 'oleinpressMedia_widgets_init' );
  */
 function oleinpressMedia_scripts() {
 	wp_enqueue_style( 'oleinpressMedia-style', get_stylesheet_uri() );
-	// wp_enqueue_script( 'oleinpressMedia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	// wp_enqueue_script( 'oleinpressMedia-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'oleinpressMedia-bootstrap-js', get_template_directory_uri() . '/lib/bootstrap/js/bootstrap.bundle.min.js', array( 'jquery' ), '4.3.1', true );
+
+	wp_enqueue_script( 'oleinpressMedia-fontawesome-js', get_template_directory_uri() . '/lib/fontawesome/js/all.js', array(), '5.3.1', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
