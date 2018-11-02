eaee6e5c63fe28ccf4c14870067908be51f0d05f
KojiKuno <koji.kuno@gmail.com>
2018-11-02 16:09:02 +0900

    固定ページからページナビゲーションを削除

--- page.php
+++ page.php
@@ -20,17 +20,7 @@ get_template_part( 'template-parts/post-image-header' ); ?>
 					while ( have_posts() ) :
 						the_post();
 						get_template_part( 'template-parts/content', 'page' );
-						?>
-					<div class="c-post-navigation row">
-						<div class="col-sm-6 c-post-navigation__previous">
-							<i class="fas fa-chevron-left"></i> <?php previous_post_link( '%link', '%title' ); ?>
-						</div>
-						<div class="col-sm-6 c-post-navigation__next">
-							<?php next_post_link( '%link', '%title' ); ?> <i class="fas fa-chevron-right"></i>
-						</div>
-					</div>
-						<?php
-//						the_post_navigation();
+
 						// If comments are open or we have at least one comment, load up the comment template.
 						if ( comments_open() || get_comments_number() ) :
 							comments_template();
